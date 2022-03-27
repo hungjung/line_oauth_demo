f3f419bbacdc5d732226a2bd538f51e28993420c
Hong Rong Zhai <johnny0214@gmail.com>
2022-03-27 18:51:31 +0800

    實作申請訂閱的callback

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use \GuzzleHttp\Client as GuzzleClient;
@@ -10,7 +11,11 @@ class NotifyController extends Controller
 {
     // 申請訂閱頁面
     public function subscribe() {
-        return view('subscribe');
+
+        $get_subsribes = DB::select('select user_name,user_access_token,created_at from subscribe where user_name=?', [session("user_id")]);
+        $cnt = count($get_subsribes);
+
+        return view('subscribe', ["cnt" => $cnt]);
     }
 
     // 送出notify訂閱認證請求
@@ -28,7 +33,49 @@ class NotifyController extends Controller
     }
 
     // notify callback url
-    public function notifycallback() {
+    public function notifycallback(Request $request, GuzzleClient $http) {
+        // 取得 authorization code
+        /**
+         * 回傳以下資料
+         * code
+         * state
+         */
+        $code = $request->code;
+        // 取得 state 和 csrf_token 比對
+        if ($request->state !== csrf_token()) {
+            return response('無效的操作！', 403);
+        }
+
+        // 透過所取得的code去取access token
+        $token_url = "https://notify-bot.line.me/oauth/token";
+        $form_data = [
+            'form_params' => [
+                'grant_type' => "authorization_code",
+                'code' => $code,
+                'redirect_uri' => env("NOTIFY_CALLBACK"),
+                'client_id' => env("NOTIFY_CLIENT_ID"),
+                'client_secret' => env("NOTIFY_CLIENT_SECRET")
+            ],
+            'http_errors' => false
+        ];
+
+        $response = $http->post($token_url, $form_data);
+
+        if ($response->getStatusCode() != 200) {
+            return view("/login", ['msg'=>$response->getBody()]);
+        }
+
+        // 成功取得access token資訊
+        /**
+         * 回傳以下資訊：
+         * acces_token 永久有效
+         */
+
+        $payload = json_decode((string)$response->getBody(), true);
+
+        DB::insert('insert into subscribe (user_name, user_access_token, created_at) values (?, ?, ?)', [session('user_id'), $payload['access_token'], Carbon::now()]);
+
+        return redirect("/subscribe");
 
     }
 
--- resources/views/subscribe.blade.php
+++ resources/views/subscribe.blade.php
@@ -12,6 +12,9 @@
         <div class="card shadow mb-4">
             <div class="card-body">
 
+            @if ($cnt)
+                <h4 class="text-success">您已訂閱本服務通知！</h4>
+            @else
                 <p>是否申請訂閱本服務通知？</p>
                 <div class="mt-4 mb-2">
                     <code>若想要申請，請點選下方按鈕連結！</code>
@@ -19,6 +22,7 @@
                 <a href="/notifyapp" class="btn btn-success btn-circle btn-lg">
                     <i class="fas fa-check"></i>
                 </a>
+            @endif
 
             </div>
         </div>
