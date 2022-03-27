3b9114c874f45a820ec7b6b24015bce03fc4cf43
Hong Rong Zhai <johnny0214@gmail.com>
2022-03-27 18:26:46 +0800

    實作notify訂閱認證請求

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -15,7 +15,16 @@ class NotifyController extends Controller
 
     // 送出notify訂閱認證請求
     public function notifyapp() {
+        $authorize_url = "https://notify-bot.line.me/oauth/authorize";
+        $query = http_build_query([
+            'response_type' => "code",
+            'client_id' => env("NOTIFY_CLIENT_ID"),
+            'redirect_uri' => env("NOTIFY_CALLBACK"),
+            'state' => csrf_token(),
+            'scope' => 'notify',
+        ]);
 
+        return redirect($authorize_url.'?'.$query);
     }
 
     // notify callback url
